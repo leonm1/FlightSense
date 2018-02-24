c2711f2a3e3a4ae93774507f3fd92d38cda1ce05
Matthew Leon <32306579+leonm1@users.noreply.github.com>
2018-02-24 03:49:10 -0600

    Date processing

--- python/processStats.py
+++ python/processStats.py
@@ -1,12 +1,68 @@
 import csv
+import os
+import datetime
 
-filename = 'ORD_STATS.csv'
+# Filepaths
+path = os.path.abspath(os.path.dirname(__file__))
 
-with open(filename, newline='') as csvfile:
-    try:
+
+for file in os.listdir(path):
+    filename = os.path.join(path, file)
+
+    # Open files
+    with open(filename, newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
+        origin = ""
+
+        # Process and parse data
         for row in reader:
-            airportcode = row.items()
-            
-    finally:
-        reader.close()
+            if len(row) == 2 and row[0].startswith("Origin"):
+                # Extract origin airport IATA Code
+                origin = row[1].split("(")[0:2]
+            elif len(row) > 1 and len(row[0]) == 2:
+                airline = row[0]
+                timeZone = None
+
+                # Parse date
+                month,day,year = row[1].split('/')
+                # Parse into int
+                year = int(year)
+                day = int(day)
+                month = int(month)
+
+                if year < 50:
+                    year += 2000
+                elif year < 100:
+                    year += 1900
+
+                # Parse time from CSV
+                schHour,schMin = row[5].split(':')
+                actHour,actMin = row[6].split(':')
+                wheelsOffHour,wheelsOffMin = row[10].split(':')
+
+
+                # Parse into int
+                schHour = int(schHour)
+                schMin = int(schMin)
+                actHour = int(actHour)
+                actMin = int(actMin)
+                wheelsOffHour = int(wheelsOffHour)
+                wheelsOffMin = int(wheelsOffMin)
+
+                # Parse date and scheduledTime into datetime object
+                scheduledTime = datetime.datetime(year, month, day, schHour, schMin, 0, 0, timeZone).timestamp()
+                actualTime = datetime.datetime(year, month, day, actHour, actMin, 0, 0, timeZone).timestamp()
+                wheelsOffTime = datetime.datetime(year, month, day, wheelsOffHour, wheelsOffMin, 0, 0, timeZone).timestamp()
+
+                predictedElapsed = row[7]
+                actualElapsed = row[8]
+
+                # timedelta obj from datetime module
+                totalDelay = actualTime - scheduledTime
+
+                
+                delayCarrier = row[12]
+                delayWeather = row[13]
+                delayNAS = row[14]
+                delaySecurity = row[15]
+                delayLateCraft = row[16]
