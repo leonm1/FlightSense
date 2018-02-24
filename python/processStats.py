9126f4aa4171a98346772e6a452f0d57a6f6e0f7
Matthew Leon <32306579+leonm1@users.noreply.github.com>
2018-02-24 09:02:07 -0600

    Refactor processStats to handle multithreading

--- python/processStats.py
+++ python/processStats.py
@@ -1,61 +1,41 @@
 import csv
 import os
 import datetime
+from multiprocessing import Pool
+import uuid
 
 # Filepaths
 path = os.path.abspath(os.path.dirname(__file__))
+# relative path to the data folder
+path += '/../data'
 
 
-for file in os.listdir(path):
-    filename = os.path.join(path, file)
 
-    # Open files
-    with open(filename, newline='') as csvfile:
-        reader = csv.reader(csvfile, delimiter=',')
-        origin = ""
+WORKERS = 10
 
-        # Process and parse data
-        for row in reader:
-            if row[0] != "YEAR":
-                # Extract origin airport IATA Code
-                origin = row[10]
-                airline = row[5]
-                timeZone = None
+# Do whatever you need to do with the row data in here
+def row_handler(row):
+    # since I used dictReader each row is now formatted in a dictionary so you can get 
+    # values by the names at the top of the columns, that'll make it much simpler
 
-                # Parse date into int
-                year = int(row[0])
-                day = int(row[2])
-                month = int(row[1])
+    # Generate row UUID
+    unique_id = uuid.uuid1()
 
-                # Process XXXX time to XX:XX time
-                CRSDep = row[13]
-                while len(CRSDep) < 4:
-                    CRSDep = "0" + CRSDep
-                delay = row[15]
+    # ex: you can just address data by their id's
+    date_obj = datetime.datetime.strptime(row["FL_DATE"], "%m/%d/%Y")
+    crs_time_obj = datetime.datetime.combine(date_obj, datetime.datetime.strptime(row["CRS_DEP_TIME"], "%H%M"))
 
-                # Parse time from CSV
-                schHour,schMin = CRSDep.split(':')
-                actHour,actMin = row[6].split(':')
-
-                # Parse into int
-                schHour = int(schHour)
-                schMin = int(schMin)
-                wheelsOffHour = int(wheelsOffHour)
-                wheelsOffMin = int(wheelsOffMin)
-
-                # Parse date and scheduledTime into datetime object
-                scheduledTime = datetime.datetime(year, month, day, schHour, schMin, 0, 0, timeZone).timestamp()
-                actualTime = datetime.datetime(year, month, day, actHour, actMin, 0, 0, timeZone).timestamp()
-
-                predictedElapsed = row[7]
-                actualElapsed = row[8]
-
-                # timedelta obj from datetime module
-                totalDelay = actualTime - scheduledTime
+    
 
 
-                delayCarrier = row[12]
-                delayWeather = row[13]
-                delayNAS = row[14]
-                delaySecurity = row[15]
-                delayLateCraft = row[16]
+if __name__ == "__main__":
+    for file in os.listdir(path):
+        filename = os.path.join(path, file)
+
+        # Open files
+        with open(filename, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+
+            pool = Pool(processes=WORKERS)
+
+            pool.map(row_handler, reader)
