93e3496f9d33f76dd74d5929796b498b942affd0
Benjamin Cooper <bencooper222@gmail.com>
2018-02-24 23:42:59 -0600

    Less logging

--- node/server.js
+++ node/server.js
@@ -2,11 +2,13 @@ const http = require('http');
 const index = require('./index.js');
 const url = require('url');
 
+let count = 0;
 //create a server object:
 http.createServer(function (req, res) {
 
-
+    if(count++ % 1000 ===0) console.log('hit: ', count);
     const query = url.parse(req.url, true).query;
+
   //  console.log(query);
     if (query.code == null || query.time == null || query.code =='' || query.time == '') {
         res.statusMessage = 400;
