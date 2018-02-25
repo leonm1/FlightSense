a57cd38960d43f6fba4cfcbfe6520b25b54e239f
Benjamin Cooper <bencooper222@gmail.com>
2018-02-24 20:54:50 -0600

    403

--- node/server.js
+++ node/server.js
@@ -5,19 +5,22 @@ const url = require('url');
 //create a server object:
 http.createServer(function (req, res) {
 
-    
+
     const query = url.parse(req.url, true).query;
     console.log(query);
-    if (query.code == null || query.time == null){
-        res.statusMessage = 403;
-        res.end();
-        return;
+    if (query.code == null || query.time == null) {
+
     }
 
     index(query.code.toUpperCase(), query.time).then((data) => {
-        res.setHeader('Content-Type','application/json');
+        res.setHeader('Content-Type', 'application/json');
         res.write(JSON.stringify(data));
         res.end();
-    })
+    }).catch(err => {
+        res.statusMessage = 403;
+        res.end();
+
+    }
+    );
 
 }).listen(80); //the server object listens on port 8080
