869d3c84e98ec559f076091540e150886ada48d3
Benjamin Cooper <bencooper222@gmail.com>
2018-02-24 20:51:27 -0600

    403

--- node/server.js
+++ node/server.js
@@ -8,6 +8,11 @@ http.createServer(function (req, res) {
     
     const query = url.parse(req.url, true).query;
     console.log(query);
+    if (query.code == null || query.time == null){
+        res.statusMessage = 403;
+        res.end();
+    }
+
     index(query.code.toUpperCase(), query.time).then((data) => {
         res.setHeader('Content-Type','application/json');
         res.write(JSON.stringify(data));
